0d89554106420ab4d8ec65da1268b463b41da74a
Demian LeClair <admin@hellofable.com>
2024-02-15 13:44:55 -0700

    works

--- imports/ui/SideBar/SideBar.svelte
+++ imports/ui/SideBar/SideBar.svelte
@@ -1,6 +1,7 @@
 <script>
     export let _state;
     import { onMount } from "svelte";
+    import TopTabs from "./Settings/TopTabs.svelte";
     import Browser from "./Browser/Browser.svelte";
     import Settings from "./Settings/Settings.svelte";
 
@@ -11,6 +12,7 @@
 </script>
 
 <sidebar class="d-flex flex-column">
+    <TopTabs />
     <Settings {_state} />
     <div id="b" class="flex-grow-1">
         <Browser {_state} {meta} />
